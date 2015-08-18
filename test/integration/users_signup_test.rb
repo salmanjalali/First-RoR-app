61244f57e83f1fc46d327c5de3fb25791f794d04
Salman <salmanjalali87@gmail.com>
2015-08-18 16:27:54 +0000

    Finish log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -20,6 +20,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert is_logged_in?
+    
   end
   
 end
